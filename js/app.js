5dca6d022127b1c7bc1041a9de3d23136f813469
Gaya Krish <gaya_krish@yahoo.com>
2017-11-06 18:31:43 -0700

    Formatted with Beautifier

--- js/app.js
+++ js/app.js
@@ -38,99 +38,99 @@ var ViewModel = function() {
                     animation: google.maps.Animation.DROP,
                 });
 
-				marker.addListener('click', function() {
-					self.setCurrentMarker(this);
-				});
-				marker.addListener('mouseover', function() {
-					self.openminiInfoWindow(this);
-				});
-				marker.addListener('mouseout', function() {
-					self.closeminiInfoWindow(this);
-				});
+                marker.addListener('click', function() {
+                    self.setCurrentMarker(this);
+                });
+                marker.addListener('mouseover', function() {
+                    self.openminiInfoWindow(this);
+                });
+                marker.addListener('mouseout', function() {
+                    self.closeminiInfoWindow(this);
+                });
 
-				self.markerArray.push(marker);
-				self.bounds.extend(marker.position);
+                self.markerArray.push(marker);
+                self.bounds.extend(marker.position);
             }
             self.map.fitBounds(self.bounds);
         } else {
-            if(self.searchText() != '') {
-            	window.alert("We could not find that location - please try again with another search term");
-            	self.searchText('');
+            if (self.searchText() != '') {
+                window.alert("We could not find that location - please try again with another search term");
+                self.searchText('');
             }
         }
     });
 
     self.openminiInfoWindow = function(marker) {
-    	var infoStr = `${marker.title}`;
-    	self.miniInfoWindow.setContent(infoStr);
-    	self.miniInfoWindow.open(map, marker);
-    	self.miniInfoWindow.addListener('closeclick', function() {
+        var infoStr = `${marker.title}`;
+        self.miniInfoWindow.setContent(infoStr);
+        self.miniInfoWindow.open(map, marker);
+        self.miniInfoWindow.addListener('closeclick', function() {
             self.miniInfoWindow.marker = null;
         });
     }
 
     self.closeminiInfoWindow = function(marker) {
-    	self.miniInfoWindow.marker = null;
+        self.miniInfoWindow.marker = null;
     }
 
     self.updateMarker = ko.computed(function() {
-    	self.request = {
-    		bounds: self.bounds,
-    		keyword: self.searchText()
-    	}
-    	self.service.nearbySearch(self.request, function(results, status) {
-	        if (status == google.maps.places.PlacesServiceStatus.OK) {
-	        	var tempMarker = [];
-	        	clearMarkers();
-	            for (var i = 0; i < results.length; i++) {
-	                var place = results[i];
-	                console.log("place " + place.name);
-	                var marker = new google.maps.Marker({
-	                    map: self.map,
-	                    position: place.geometry.location,
-	                    title: place.name,
-	                    animation: google.maps.Animation.DROP,
-	                });
+        self.request = {
+            bounds: self.bounds,
+            keyword: self.searchText()
+        }
+        self.service.nearbySearch(self.request, function(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var tempMarker = [];
+                clearMarkers();
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    console.log("place " + place.name);
+                    var marker = new google.maps.Marker({
+                        map: self.map,
+                        position: place.geometry.location,
+                        title: place.name,
+                        animation: google.maps.Animation.DROP,
+                    });
 
-	                marker.addListener('click', function() {
-						self.setCurrentMarker(this);
-					});
-					marker.addListener('mouseover', function() {
-						self.openminiInfoWindow(this);
-					});
-					marker.addListener('mouseout', function() {
-						self.closeminiInfoWindow(this);
-					});
+                    marker.addListener('click', function() {
+                        self.setCurrentMarker(this);
+                    });
+                    marker.addListener('mouseover', function() {
+                        self.openminiInfoWindow(this);
+                    });
+                    marker.addListener('mouseout', function() {
+                        self.closeminiInfoWindow(this);
+                    });
 
-	                tempMarker.push(marker);
-	                self.bounds.extend(marker.position);
-	            }
-	            self.markerArray(tempMarker);
-	            self.map.fitBounds(self.bounds);
+                    tempMarker.push(marker);
+                    self.bounds.extend(marker.position);
+                }
+                self.markerArray(tempMarker);
+                self.map.fitBounds(self.bounds);
 
-	        } else {
-	        	if(self.searchText() != '') {
-            		window.alert("We could not find that location - please try again with another search term");
-                  	self.searchText('');
-            	}
-        	}
-	    });
+            } else {
+                if (self.searchText() != '') {
+                    window.alert("We could not find that location - please try again with another search term");
+                    self.searchText('');
+                }
+            }
+        });
     });
 
     self.setCurrentMarker = function(marker) {
-    	self.currentMarker(marker);
-    	console.log("currentMarker " + self.currentMarker().title);
-    	toggleMarker(self.currentMarker());
-    	self.openminiInfoWindow(self.currentMarker());
-    	//TODO: zoom in map and setcenter around current marker
+        self.currentMarker(marker);
+        console.log("currentMarker " + self.currentMarker().title);
+        toggleMarker(self.currentMarker());
+        self.openminiInfoWindow(self.currentMarker());
+        //TODO: zoom in map and setcenter around current marker
     };
 
     var toggleMarker = function(marker) {
-    	if (marker.getAnimation() !== null) {
-			marker.setAnimation(null);
-		} else {
-			marker.setAnimation(google.maps.Animation.DROP);
-		}
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.DROP);
+        }
     }
 
     var detailedInfoWindow = function() {
