e51590395d8800dd3f176c4123b17cad5f878c57
Gaya Krish <gaya_krish@yahoo.com>
2017-11-06 18:28:09 -0700

    Added mini informarker on mouse over and click, marker animations, search functionality and its corresponding markers, search list items updated correctly.

--- js/app.js
+++ js/app.js
@@ -1,98 +1,147 @@
-var markerArray = [];
-var filterArray = ['Restaurants', 'Schools', 'University', 'Bus', 'Museum', 'Park'];
-var currentMarker;
-
 var ViewModel = function() {
     var self = this;
-    var geocoder = new google.maps.Geocoder();
-
-    self.markerArray = ko.observableArray(markerArray);
-    self.bounds = new google.maps.LatLngBounds();
+    self.markerArray = ko.observableArray([]);
     self.searchText = ko.observable('');
-
-    var location = {
+    self.location = {
         lat: 40.585258,
         lng: -105.084419
     };
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 15,
+    self.bounds = new google.maps.LatLngBounds(self.location);
+    self.map = new google.maps.Map(document.getElementById('map'), {
+        center: self.location,
+        zoom: 25,
         mapTypeControl: false,
-        mapType: 'normal'
+        mapType: 'normal',
+        preserveViewport: false
     });
-
-
-    var request = {
-        location: location,
-        radius: '2000',
-        types: ['school', 'museum', 'restaurant', 'park', 'university', 'bus_station']
+    self.request = {
+        location: self.location,
+        radius: '10000',
+        types: ['museum', 'park', 'university', 'art_gallery']
     };
+    self.service = new google.maps.places.PlacesService(self.map);
+    self.currentMarker = ko.observable('');
+    self.detailedInfoWindow = new google.maps.InfoWindow();
+    self.miniInfoWindow = new google.maps.InfoWindow();
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, function(results, status) {
+    //TODO: autocomplete
+    // self.autocomplete = new google.maps.places.Autocomplete(self.searchText());
+
+    self.service.nearbySearch(self.request, function(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
                 var marker = new google.maps.Marker({
-                    map: map,
+                    map: self.map,
                     position: place.geometry.location,
-                    title: place.name
+                    title: place.name,
+                    animation: google.maps.Animation.DROP,
                 });
 
-                self.markerArray.push(marker);
-                self.bounds.extend(marker.position);
-            }
-            map.fitBounds(self.bounds);
+				marker.addListener('click', function() {
+					self.setCurrentMarker(this);
+				});
+				marker.addListener('mouseover', function() {
+					self.openminiInfoWindow(this);
+				});
+				marker.addListener('mouseout', function() {
+					self.closeminiInfoWindow(this);
+				});
 
+				self.markerArray.push(marker);
+				self.bounds.extend(marker.position);
+            }
+            self.map.fitBounds(self.bounds);
         } else {
-            //TODO: handle error
+            if(self.searchText() != '') {
+            	window.alert("We could not find that location - please try again with another search term");
+            	self.searchText('');
+            }
         }
     });
 
-    self.filterArrayVisible = ko.observable(false);
-    self.toggleFilterArray = function() {
-        self.filterArrayVisible(!self.filterArrayVisible());
+    self.openminiInfoWindow = function(marker) {
+    	var infoStr = `${marker.title}`;
+    	self.miniInfoWindow.setContent(infoStr);
+    	self.miniInfoWindow.open(map, marker);
+    	self.miniInfoWindow.addListener('closeclick', function() {
+            self.miniInfoWindow.marker = null;
+        });
     }
 
-    self.filterArray = ko.observableArray(filterArray);
-    self.currentFilter = ko.observable('Restaurants');
-
-    self.currentFilter = ko.computed(function() {
-        document.getElementById
-    });
+    self.closeminiInfoWindow = function(marker) {
+    	self.miniInfoWindow.marker = null;
+    }
 
     self.updateMarker = ko.computed(function() {
-        geocoder.geocode({
-            'address': self.searchText()
-        }, function(results, status) {
-            if (status == 'OK') {
-                var place = results[0];
-                clearMarkers();
-                map.setCenter(place.geometry.location);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: place.geometry.location,
-                    title: place.name
-                });
-                self.markerArray.push(marker);
-                self.bounds.extend(marker.position);
-                map.fitBounds(self.bounds);
-                // }
-            } else {
-                console.dir('Geocode was not successful for the following reason: ' + status);
-            }
-        });
+    	self.request = {
+    		bounds: self.bounds,
+    		keyword: self.searchText()
+    	}
+    	self.service.nearbySearch(self.request, function(results, status) {
+	        if (status == google.maps.places.PlacesServiceStatus.OK) {
+	        	var tempMarker = [];
+	        	clearMarkers();
+	            for (var i = 0; i < results.length; i++) {
+	                var place = results[i];
+	                console.log("place " + place.name);
+	                var marker = new google.maps.Marker({
+	                    map: self.map,
+	                    position: place.geometry.location,
+	                    title: place.name,
+	                    animation: google.maps.Animation.DROP,
+	                });
+
+	                marker.addListener('click', function() {
+						self.setCurrentMarker(this);
+					});
+					marker.addListener('mouseover', function() {
+						self.openminiInfoWindow(this);
+					});
+					marker.addListener('mouseout', function() {
+						self.closeminiInfoWindow(this);
+					});
+
+	                tempMarker.push(marker);
+	                self.bounds.extend(marker.position);
+	            }
+	            self.markerArray(tempMarker);
+	            self.map.fitBounds(self.bounds);
+
+	        } else {
+	        	if(self.searchText() != '') {
+            		window.alert("We could not find that location - please try again with another search term");
+                  	self.searchText('');
+            	}
+        	}
+	    });
     });
+
+    self.setCurrentMarker = function(marker) {
+    	self.currentMarker(marker);
+    	console.log("currentMarker " + self.currentMarker().title);
+    	toggleMarker(self.currentMarker());
+    	self.openminiInfoWindow(self.currentMarker());
+    	//TODO: zoom in map and setcenter around current marker
+    };
+
+    var toggleMarker = function(marker) {
+    	if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.DROP);
+		}
+    }
+
+    var detailedInfoWindow = function() {
+
+    }
 
     var clearMarkers = function() {
         console.log("clearing markers");
-        for (var i = 0; i < self.markerArray.length; i++) {
-            self.markerArray[i].setMap(null);
+        for (var i = 0; i < self.markerArray().length; i++) {
+            self.markerArray()[i].setMap(null);
         }
-        self.markerArray = [];
-        console.log("markerArray.length " + markerArray.length);
-
     }
 }
 
