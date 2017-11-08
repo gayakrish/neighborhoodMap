8797fd861facf09e13f353dceb2f424f9ed5af5d
Gaya Krish <gaya_krish@yahoo.com>
2017-11-07 18:23:32 -0700

    Added Detailed InfoWindow using FourSquare API

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var ViewModel = function() {
     self.markerArray = ko.observableArray([]);
     self.searchText = ko.observable('');
     self.location = {
-        lat: 40.585258,
-        lng: -105.084419
+        lat: 40.5749824,
+        lng: -105.0838675
     };
     self.bounds = new google.maps.LatLngBounds(self.location);
     self.map = new google.maps.Map(document.getElementById('map'), {
@@ -16,8 +16,8 @@ var ViewModel = function() {
     });
     self.request = {
         location: self.location,
-        radius: '10000',
-        types: ['museum', 'park', 'university', 'art_gallery']
+        radius: '11000',
+        types: ['museum', 'park', 'university']
     };
     self.service = new google.maps.places.PlacesService(self.map);
     self.currentMarker = ko.observable('');
@@ -120,12 +120,13 @@ var ViewModel = function() {
     self.setCurrentMarker = function(marker) {
         self.currentMarker(marker);
         console.log("currentMarker " + self.currentMarker().title);
-        toggleMarker(self.currentMarker());
-        self.openminiInfoWindow(self.currentMarker());
+        self.toggleMarker(self.currentMarker());
+        // self.openminiInfoWindow(self.currentMarker());
+        self.detailedInfoWindow(marker);
         //TODO: zoom in map and setcenter around current marker
     };
 
-    var toggleMarker = function(marker) {
+    self.toggleMarker = function(marker) {
         if (marker.getAnimation() !== null) {
             marker.setAnimation(null);
         } else {
@@ -133,11 +134,136 @@ var ViewModel = function() {
         }
     }
 
-    var detailedInfoWindow = function() {
+    self.detailedInfoWindow = function(marker) {
+        const venueIdRequest = new XMLHttpRequest();
+        venueIdRequest.onload = self.getVenueId;
+        venueIdRequest.onerror = function(err) {
+            console.log("Error getting venue Id " + err);
+        }
 
+        var markerPos = marker.getPosition();
+        var markerLat = markerPos.lat();
+        var markerLng = markerPos.lng();
+        console.log("marker position " + markerLat + " , " + markerLng);
+        var markerName = marker.title;
+        venueIdRequest.open('GET',
+            `https://api.foursquare.com/v2/venues/search?ll=${markerLat},${markerLng}&name=${markerName}
+                            &client_id=EDQTERY1IPD34QQA4NGCV0OI3QGKMQQM5SCSFXX04YDRIRPE
+                            &client_secret=NCOEULIZXHH04MBAZTJDLUZIOF2D1YFKCCUP5OZEIX4CM1OU
+                            &v=20170101`);
+        venueIdRequest.send();
     }
 
-    var clearMarkers = function() {
+    self.getVenueId = function() {
+        var data = JSON.parse(this.response);
+        console.log(data.response.venues);
+        var venues = data.response.venues;
+
+        for (var i = 0; i < venues.length; i++) {
+            var venueName = venues[i].name;
+            var markerTitle = self.currentMarker().title;
+
+            if (self.checkVenueName(venueName, markerTitle)) {
+
+                var venueId = venues[i].id;
+
+                console.log("item id : " + venueId);
+                console.log("name " + venueName);
+
+
+                const venueDetailsRequest = new XMLHttpRequest();
+                venueDetailsRequest.onload = self.venueDetails;
+                venueDetailsRequest.onerror = function(err) {
+                    console.log("Error getting venue details " + err);
+                }
+
+                venueDetailsRequest.open('GET', `https://api.foursquare.com/v2/venues/${venueId}?client_id=EDQTERY1IPD34QQA4NGCV0OI3QGKMQQM5SCSFXX04YDRIRPE
+                                    &client_secret=NCOEULIZXHH04MBAZTJDLUZIOF2D1YFKCCUP5OZEIX4CM1OU&v=20170101`);
+                venueDetailsRequest.send();
+            }
+        }
+    }
+
+    self.checkVenueName = function(venueName, markerTitle) {
+        var venueArr = venueName.split(" ");
+        var markerTitleArr = markerTitle.split(" ");
+        if (venueName.includes(markerTitle)) {
+            return true;
+        } else {
+            for (var i = 0; i < venueArr.length; i++) {
+                for (var j = 0; j < markerTitleArr.length; j++) {
+                    if (venueArr[i].includes(markerTitleArr[j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    self.venueDetails = function() {
+        var marker = self.currentMarker();
+        console.log("venue data");
+        var data = JSON.parse(this.response);
+        console.log(data);
+        var venueUrl = self.getVenueUrl(data);
+        var venueHours = self.getVenueHours(data);
+        var venueLoc = self.getVenueLoc(data);
+        var venuePic = self.getVenuePic(data);
+        var infoStr = `<div> <p> ${marker.title} </p>
+                        <p> ${venueLoc} </p>
+                        <img src=${venuePic}>
+                        <a href=${venueUrl}> ${venueUrl} </a>
+                        <p> ${venueHours} </p>
+                        </div>`;
+        self.miniInfoWindow.setContent(infoStr);
+        self.miniInfoWindow.open(map, marker);
+        self.miniInfoWindow.addListener('closeclick', function() {
+            self.miniInfoWindow.marker = null;
+        });
+    }
+
+    self.getVenuePic = function(data) {
+        if (typeof(data.response.venue.bestPhoto) != 'undefined') {
+            return `${data.response.venue.bestPhoto.prefix}50x50${data.response.venue.bestPhoto.suffix}`;
+        } else {
+            return data.response.venue.canonicalUrl;
+        }
+    }
+
+    self.getVenueUrl = function(data) {
+        if (typeof(data.response.venue.url) != 'undefined') {
+            return data.response.venue.url;
+        } else {
+            return data.response.venue.canonicalUrl;
+        }
+    }
+
+    self.getVenueLoc = function(data) {
+        if (typeof(data.response.venue.location.address) != 'undefined') {
+            return data.response.venue.location.address;
+        } else {
+            return "";
+        }
+    }
+
+    self.getVenueHours = function(data) {
+        if (typeof(data.response.venue.hours) != 'undefined') {
+            return data.response.venue.hours.status;
+        } else {
+            return "";
+        }
+    }
+
+    self.getVenueImage = function(data) {
+        if (typeof(data.response.venue.url) != 'undefined') {
+            return data.response.venue.url;
+        } else {
+            return data.response.venue.canonicalUrl;
+        }
+    }
+
+    self.clearMarkers = function() {
         console.log("clearing markers");
         for (var i = 0; i < self.markerArray().length; i++) {
             self.markerArray()[i].setMap(null);
